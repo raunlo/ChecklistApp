f54620728802bbcb5608a900ee7572e3d2d5ec59
Rauno Lõhmus <32277590+raunlo@users.noreply.github.com>
2022-09-19 04:29:00 +0200

    add auto deployment (#9)

---
+++ src/main/java/com/raunlo/checklist/config/DatabaseHealthCheck.java
@@ -0,0 +1,42 @@
+package com.raunlo.checklist.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Liveness
+@ApplicationScoped
+@Slf4j
+public class DatabaseHealthCheck implements HealthCheck {
+    private final DataSource dataSource;
+
+    @Inject()
+    public DatabaseHealthCheck(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Database up")
+                .status(isDatabaseAccepting())
+                .build();
+    }
+
+    private boolean isDatabaseAccepting() {
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT 1 from task")) {
+                return statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
