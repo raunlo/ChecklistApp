df061fd416ed275bc8c3c0bc2a68d36add20fced
Rauno Lohmus <rlohmus@twilio.com>
2022-11-06 19:28:34 +0100

    it tests

--- src/test/java/com/raunlo/checklist/ChecklistFunctionalIT.java
+++ src/test/java/com/raunlo/checklist/ChecklistFunctionalIT.java
@@ -1,49 +1,66 @@
 package com.raunlo.checklist;
 
-
 import com.raunlo.checklist.core.entity.Checklist;
+import com.raunlo.checklist.core.entity.Task;
 import io.helidon.microprofile.tests.junit5.Configuration;
 import io.helidon.microprofile.tests.junit5.HelidonTest;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.GenericType;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.Collection;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @HelidonTest
 @Configuration(configSources = "application.yaml")
-public class ChecklistFunctionalIT {
-
-    @Inject
-    private WebTarget webTarget;
+public class ChecklistFunctionalIT extends CommonChecklistOperations {
 
     @Test
-    public void test() {
-        Checklist checklist = new Checklist()
-                .withName("test checklist");
-        final Response response = webTarget.path("/api/v1/checklist")
-                .request()
-                .post(Entity.entity(checklist, MediaType.APPLICATION_JSON_TYPE));
-        assertThat(response.getStatus() == 201);
-        final Checklist responseAsEntity = response.readEntity(Checklist.class);
-        assertThat(responseAsEntity.getId()).isNotNull();
-        assertThat(responseAsEntity.getName().equals(checklist.getName()));
-        assertThat(responseAsEntity.getTasks()).matches(tasks -> tasks.size() == 0);
+    public void successfullyCreatesTask() {
+        final Checklist createdChecklist = createChecklist(new Checklist()
+                .withName("creates_task"));
+        final Task createsTask = successfullyCreatesTask(createdChecklist.getId(), new Task()
+                .withName("new task"));
+        final Response getTask = getTask(createdChecklist.getId(), createsTask.getId());
+        assertThat(getTask)
+                .matches(response -> response.getStatus() == 200);
 
-        final Response getResponse = webTarget.path("/api/v1/checklist")
-                .request()
-                .get();
+        assertThat(getTask.readEntity(Task.class))
+                .matches((Task existingTask) -> existingTask.isCompleted() != createsTask.isCompleted())
+                .matches(existingTask -> existingTask.getName().equals(createsTask.getName()))
+                .matches(existingTask -> existingTask.getId().equals(createsTask.getId()));
+    }
 
-        final Collection<Checklist> checklists = getResponse.readEntity(new GenericType<Collection<Checklist>>() {
-        });
+    @Test
+    public void successfullyDeleteTask() {
+        final Checklist createdChecklist = createChecklist(new Checklist().withName("deletes_task"));
+        final Task createdTask = successfullyCreatesTask(createdChecklist.getId(), new Task().withName("new task"));
+        deleteTask(createdChecklist.getId(), createdTask.getId());
+        assertThat(getTask(createdChecklist.getId(), createdTask.getId()))
+                .matches(response -> response.getStatus() == 404);
+    }
 
-        System.out.println(checklists);
+    @Test
+    public void successfullyFindsAllTasks() {
+        final Checklist createdChecklist = createChecklist(new Checklist().withName("finds_all_tasks"));
+        final Task createdTask1 = successfullyCreatesTask(createdChecklist.getId(), new Task().withName("new task"));
+        final Task createdTask2 = successfullyCreatesTask(createdChecklist.getId(), new Task().withName("new task2"));
+        final Task createdTask3 = successfullyCreatesTask(createdChecklist.getId(), new Task().withName("new task3"));
+
+        final Collection<Task> allTasks = getAllTasks(createdChecklist.getId());
+        assertThat(allTasks).contains(createdTask3, createdTask1, createdTask2);
+    }
+
+    @Test
+    public void successfullyUpdatesTask() {
+        final Checklist createdChecklist = createChecklist(new Checklist().withName("finds_all_tasks"));
+        final Task createdTask = successfullyCreatesTask(createdChecklist.getId(), new Task().withName("new task"));
+
+        updateTask(createdChecklist.getId(), createdTask.withCompleted(true));
+        final Response getTask = getTask(createdChecklist.getId(), createdTask.getId());
+        assertThat(getTask.getStatus()).isEqualTo(200);
+        assertThat(getTask.readEntity(Task.class))
+                .matches(Task::isCompleted);
+
     }
 }
---
+++ src/test/java/com/raunlo/checklist/CommonChecklistOperations.java
@@ -0,0 +1,85 @@
+package com.raunlo.checklist;
+
+import com.raunlo.checklist.core.entity.Checklist;
+import com.raunlo.checklist.core.entity.Task;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CommonChecklistOperations {
+
+    private static final String BASE_PATH = "/api/v1/checklist/";
+    @Inject
+    WebTarget webTarget;
+
+    public Checklist createChecklist(Checklist checklist) {
+        final Response response = webTarget.path(BASE_PATH)
+                .request()
+                .post(Entity.entity(checklist, MediaType.APPLICATION_JSON_TYPE));
+        assertThat(response.getStatus() == 201);
+        final Checklist savedChecklist = response.readEntity(Checklist.class);
+        assertThat(savedChecklist.getId()).isNotNull();
+        assertThat(savedChecklist.getName()).isEqualTo(checklist.getName());
+        assertThat(savedChecklist.getTasks()).matches(tasks -> tasks.size() == 0);
+        assertThat(webTarget.getUri().toString() + BASE_PATH + savedChecklist.getId())
+                .isEqualTo(response.getHeaderString("Location").replace("127.0.0.1", "localhost"));
+
+        return savedChecklist;
+    }
+
+    public Task successfullyCreatesTask(long checklistId, Task task) {
+        final Response response = webTarget.path(BASE_PATH + checklistId + "/task")
+                .request()
+                .post(Entity.entity(task, MediaType.APPLICATION_JSON_TYPE));
+        final Task createdTask = response.readEntity(Task.class);
+
+        assertThat(response.getStatus()).isEqualTo(201);
+        assertThat(createdTask.getId()).isNotNull();
+        assertThat(createdTask.getName()).isEqualTo(task.getName());
+        assertThat(createdTask.isCompleted()).isEqualTo(task.isCompleted());
+        assertThat(webTarget.getUri().toString() + BASE_PATH + checklistId + "/task/" + createdTask.getId());
+
+        return createdTask;
+    }
+
+    public Response getTask(long checklistId, long taskId) {
+        return webTarget.path(BASE_PATH + checklistId + "/task/" + taskId)
+                .request()
+                .get();
+    }
+
+    public void deleteTask(long checklistId, long taskId) {
+        final Response response = webTarget.path(BASE_PATH + checklistId + "/task/" + taskId)
+                .request()
+                .delete();
+
+        assertThat(response.getStatus()).isEqualTo(204);
+    }
+
+    public Collection<Task> getAllTasks(long checklistId) {
+        final Response response = webTarget.path(BASE_PATH + checklistId + "/task")
+                .request()
+                .get();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        return response.readEntity(new GenericType<>() {
+        });
+    }
+
+    public Task updateTask(long checklistId, Task task) {
+        final Response response = webTarget.path(BASE_PATH + checklistId + "/task/" + task.getId())
+                .request()
+                .put(Entity.entity(task, MediaType.APPLICATION_JSON_TYPE));
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        return response.readEntity(Task.class);
+    }
+}
